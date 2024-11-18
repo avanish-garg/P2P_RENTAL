73a9f7f1986393653997031ec30a6b3a674555bf
Avanish Garg <avanishgarg6734@gmail.com>
2024-11-18 18:01:43 +0530

    hardhat reinstalled

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config();
 
+/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.27",
-  networks: {
-    amoy: {
-      url: process.env.AMOY_RPC_URL,
-      accounts: [process.env.PRIVATE_KEY],
-    },
-  },
-  etherscan: {
-    apiKey: process.env.POLYGONSCAN_API_KEY,
-  },
 };
