e3231ff50ea71278bc3a47c5fff9a0cba2f59a78
Avanish Garg <avanishgarg6734@gmail.com>
2024-11-23 01:19:04 +0530

    Frontend Setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import RentalForm from "./components/RentalForm";
+import RentalList from "./components/RentalList";
 
 function App() {
     return (
         <div>
-            <h1>Welcome to the P2P Rental System</h1>
+            <h1>Rental NFT Platform</h1>
+            <RentalForm />
+            <RentalList />
         </div>
     );
 }
